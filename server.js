46093efb360a36b6be3e69e01c2916d55b63bcf0
Jonathans0ramirez <Jonathan200949@gmail.com>
2019-10-17 01:17:41 -0500

    updated to deploy to heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 //Serve only the static files from the dist directory
-app.use(express.static(_dirname +'/dist/SpringBootFront'));
+app.use(express.static(__dirname +'/dist/SpringBootFront'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(_dirname+'/dist/SpringBootFront/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/SpringBootFront/index.html'));
 });
 
 //Start the app by listening on the default Heroku port
